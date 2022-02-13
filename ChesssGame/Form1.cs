f049bbe44be911bf8b827f34d46e47eb8b05dad0
steven1005 <ys.liang105@gmail.com>
2022-02-13 12:44:56 +0800

    2022/02/13 ys v1.0.0.0 First Upload

---
+++ ChesssGame/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChesssGame
+{
+    public partial class Form1 : Form
+    {
+
+        // Accessing Form's Controls from another class
+        // Static form. Null if no form created yet.
+        private static Form1 form = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            ChnangeMainFormBoard(Global.iBordType);
+            Global.piece.PaintPieceIcon();
+            StartGame(Global.iBordType);
+            form = this;
+            Global.TurnPlayPiece();
+        }
+
+        // Accessing Form's Controls from another class
+        private delegate void ChangePieceDelegate();
+        private delegate void StatusMsgDelegate(string sMsg);
+        // Accessing Form's Controls from another class
+        // Static method, call the non-static version if the form exist.
+        public static void ChangePieceRadioValue()
+        {
+            if (form != null)
+                form.ChangePieceRadio();
+        }
+
+        public static void ChangeStatusMessage(string sMsg)
+        {
+            if (form != null)
+                form.UpdateStatusMessage(sMsg);
+        }
+
+        // Accessing Form's Controls from another class
+        private void ChangePieceRadio()
+        {
+            // If this returns true, it means it was called from an external thread.
+            if (InvokeRequired)
+            {
+                // Create a delegate of this method and let the form run it.
+                this.Invoke(new ChangePieceDelegate(ChangePieceRadio), new object[] { });
+                return; // Important
+            }
+            // Set textBox
+            //textBox1.Enabled = enable;
+            statusPiecelb.Text = (Global.bPieceStep) ? "紅棋下     " : "黑棋下     ";
+        }
+
+        private void UpdateStatusMessage(string sMsg)
+        {
+            if (InvokeRequired)
+            {
+                // Create a delegate of this method and let the form run it.
+                this.Invoke(new StatusMsgDelegate(UpdateStatusMessage), new object[] { sMsg });
+                return; // Important
+            }
+            statusDesplb.Text = sMsg;
+        }
+
+        private void ChnangeMainFormBoard(int iType)
+        {
+            int iWidth = Properties.Resources.HalfBoard.Width;
+            int iHeight = Properties.Resources.HalfBoard.Height;
+            // 暗棋 半板
+            if (iType == (int)GameBoard.HalfBoard)
+            {
+                this.MainPanel.BackgroundImage = Properties.Resources.HalfBoard;
+            }
+            // 象棋 全板
+            if (iType == (int)GameBoard.WholeBoard)
+            {
+                this.MainPanel.BackgroundImage = Properties.Resources.FullBoard;
+                iWidth = Properties.Resources.FullBoard.Width;
+                iHeight = Properties.Resources.FullBoard.Height - 60;
+            }
+            this.Width = iWidth - 50;
+            this.Height = iHeight + 80;
+            this.MainPanel.Width = iWidth;
+            this.MainPanel.Height = iHeight;
+        }
+
+        private void 暗棋ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Global.iBordType = 0;
+            ChnangeMainFormBoard(Global.iBordType);
+            StartGame(Global.iBordType);
+        }
+
+        private void 象棋ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Global.iBordType = 1;
+            ChnangeMainFormBoard(Global.iBordType);
+            StartGame(Global.iBordType);
+        }
+
+        // 暗棋 亂數放棋子
+        public void RandomPiece()
+        {
+            Random rnd = new Random();  //產生亂數初始值
+            for (int i = 0; i < 32; i++)    
+            {
+                //board.rectHalfBoard[i].iPieceIdx = (int)(rnd.Next(1, 32));   //亂數產生，亂數產生的範圍是1~9
+                Board.HalfBoardStatus bi = Global.board.rectHalfBoard[i];
+                bi.iPieceIdx = (int)(rnd.Next(0, 32));
+                //Console.WriteLine(bi.iPieceIdx);
+                for (int j = 0; j < i; j++)
+                {
+                    while (Global.board.rectHalfBoard[j].iPieceIdx == Global.board.rectHalfBoard[i].iPieceIdx)    //檢查是否與前面產生的數值發生重複，如果有就重新產生
+                    {
+                        j = 0;  //如有重複，將變數j設為0，再次檢查 (因為還是有重複的可能)
+                        //board.rectHalfBoard[i].iPieceIdx = (int)(rnd.Next(1, 32));   //亂數產生，亂數產生的範圍是1~9
+                        Board.HalfBoardStatus bj = Global.board.rectHalfBoard[i];
+                        bj.iPieceIdx = (int)(rnd.Next(0, 32));
+                    }
+                } // end for j
+                //Console.WriteLine(bi.iPieceIdx);
+            } // end for i
+        }
+
+        private void StartGame(int iType)
+        {
+            if (iType == (int)GameBoard.HalfBoard)
+            {
+                // 亂數放棋子
+                RandomPiece();
+
+                for (int i = 0; i < 16; i++)
+                {
+                    //piece.PlayOne[i].pic.Location = board.rectHalfBoard[i].rect.Location;
+                    //this.MainPanel.Controls.Add(piece.PlayOne[i].pic);
+                    Board.HalfBoardStatus iboard = Global.board.rectHalfBoard[i];
+                    Global.piece.PlayOne[i].pic.Location = Global.board.rectHalfBoard[iboard.iPieceIdx].rect.Location;
+                    Global.board.rectHalfBoard[iboard.iPieceIdx].iPlayer = 0;
+                    Global.board.rectHalfBoard[iboard.iPieceIdx].iPieceIdx = i;
+                    // 改蓋牌
+                    Global.piece.PlayOne[i].pic.Visible = false;
+                    this.MainPanel.Controls.Add(Global.piece.PlayOne[i].pic);
+                }
+
+                for (int i = 16; i < 32; i++)
+                {
+                    //piece.PlayTwo[i - 16].pic.Location = board.rectHalfBoard[i].rect.Location;
+                    //this.MainPanel.Controls.Add(piece.PlayTwo[i - 16].pic);
+                    Board.HalfBoardStatus iboard = Global.board.rectHalfBoard[i];
+                    Global.piece.PlayTwo[i - 16].pic.Location = Global.board.rectHalfBoard[iboard.iPieceIdx].rect.Location;
+                    Global.board.rectHalfBoard[iboard.iPieceIdx].iPlayer = 1;
+                    Global.board.rectHalfBoard[iboard.iPieceIdx].iPieceIdx = i - 16;
+                    // 改蓋牌
+                    Global.piece.PlayTwo[i - 16].pic.Visible = false;
+                    this.MainPanel.Controls.Add(Global.piece.PlayTwo[i - 16].pic);
+                }
+            }
+        }
+
+        private void 開始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartGame(Global.iBordType);
+        }
+
+        private void MainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (Global.iBordType == (int)GameBoard.HalfBoard)
+            {
+                for(int i = 0; i < 32 ; i++)
+                {
+                    if (Global.board.rectHalfBoard[i].iPlayer != -1)
+                    {
+                        // Create image.
+                        Image newImage = Properties.Resources.Dark;
+
+                        // Create point for upper-left corner of image.
+                        PointF ulCorner = Global.board.rectHalfBoard[i].rect.Location;
+
+                        // Draw image to screen.
+                        e.Graphics.DrawImage(newImage, ulCorner);
+                    }
+                }
+            } // end HalfBoard
+        }
+
+        private void MainPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Global.iBordType == (int)GameBoard.HalfBoard)
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    Board.HalfBoardStatus iboard = Global.board.rectHalfBoard[i];
+                    if (iboard.rect.Contains(e.X, e.Y))
+                    {
+                        if (Global.board.rectHalfBoard[i].iPlayer == 0)
+                        {
+                            for(int i1 = 0; i1 < 16; i1++)
+                            {
+                                if (iboard.rect.Contains(Global.piece.PlayOne[i1].pic.Location.X, Global.piece.PlayOne[i1].pic.Location.Y))
+                                {
+                                    Global.piece.PlayOne[i1].pic.Visible = true;
+                                    Global.piece.PlayOne[i1].ePieceSatus = Piece.SelectedType.None;
+                                    Global.TurnPlayPiece();
+                                    Global.MsgStatusUpdate("翻棋 紅 " + Global.piece.PlayOne[i1].sName);
+                                }
+                            }
+                        }
+
+                        if (Global.board.rectHalfBoard[i].iPlayer == 1)
+                        {
+                            for (int i1 = 0; i1 < 16; i1++)
+                            {
+                                if (iboard.rect.Contains(Global.piece.PlayTwo[i1].pic.Location.X, Global.piece.PlayTwo[i1].pic.Location.Y))
+                                {
+                                    Global.piece.PlayTwo[i1].pic.Visible = true;
+                                    Global.piece.PlayTwo[i1].ePieceSatus = Piece.SelectedType.None;
+                                    Global.TurnPlayPiece();
+                                    Global.MsgStatusUpdate("翻棋 黑 " + Global.piece.PlayTwo[i1].sName);
+                                }
+                            }
+                        }
+                    } // Contain Rect
+                } // end for
+
+                //for(int iOne = 0; iOne < 16; iOne++)
+                //{
+                //    Rectangle rect = new Rectangle(Global.piece.PlayOne[iOne].pic.Location, Global.piece.PlayOne[iOne].pic.Size);
+                //    if (rect.Contains(e.X, e.Y))
+                //    {
+                //        Console.WriteLine("Two " + Global.piece.PlayOne[iOne].sName);
+                //    }
+                //}
+            } // end HalfBoard
+            MainPanel.Invalidate();
+        }
+    }
+}
---
+++ ChesssGame/Piece.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ChesssGame
+{
+    public class Piece
+    {
+        public enum SelectedType
+        {
+            Dark = 0,
+            None,
+            Selected,
+            aLive
+        }
+        
+        public bool bFight = false;
+        picPiece[] FightSeletedPiece = new picPiece[2]; //  0 第一個選擇 1 第一個選擇
+
+        public static Size sPieceIcon = new Size(74, 76);
+        public class picPiece
+        {
+            public PictureBox pic;
+            public string sName; // 將 士 象 車 馬 炮 卒
+                                 // 帥 仕 相 車 馬 炮 兵
+            public int iLevel; // 0 將 1 士 1 士 2 象 2 象 3 車 3 車 4 馬 4 馬 5 炮 5 炮 6 卒 6 卒 6 卒 6 卒 6 卒 
+                               // 0 帥 1 仕 1 仕 2 相 2 相 3 車 3 車 4 馬 4 馬 5 炮 5 炮 6 兵 6 兵 6 兵 6 兵 6 兵
+            public SelectedType ePieceSatus;
+        }
+
+        public Piece()
+        {
+            InitPictureBox();
+        }
+
+        // 紅
+        public List <picPiece> PlayOne = new List<picPiece> {
+            new picPiece { pic = new PictureBox(), sName = "帥", iLevel = 0, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "仕", iLevel = 1, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "仕", iLevel = 1, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "相", iLevel = 2, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "相", iLevel = 2, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "車", iLevel = 3, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "車", iLevel = 3, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "馬", iLevel = 4, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "馬", iLevel = 4, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "炮", iLevel = 5, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "炮", iLevel = 5, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "兵", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "兵", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "兵", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "兵", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "兵", iLevel = 6, ePieceSatus = SelectedType.Dark}
+            };
+        
+        // 黑
+        public List<picPiece> PlayTwo = new List<picPiece> {
+            new picPiece { pic = new PictureBox(), sName = "將", iLevel = 0, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "士", iLevel = 1, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "士", iLevel = 1, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "象", iLevel = 2, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "象", iLevel = 2, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "車", iLevel = 3, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "車", iLevel = 3, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "馬", iLevel = 4, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "馬", iLevel = 4, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "炮", iLevel = 5, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "炮", iLevel = 5, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "卒", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "卒", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "卒", iLevel = 6, ePieceSatus = SelectedType.Dark},
+            new picPiece { pic = new PictureBox(), sName = "卒", iLevel = 6, ePieceSatus = SelectedType.Dark}, 
+            new picPiece { pic = new PictureBox(), sName = "卒", iLevel = 6, ePieceSatus = SelectedType.Dark}
+            };
+
+        public void InitPictureBox()
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                if (PlayOne[i].pic != null)
+                {
+                    PlayOne[i].pic.Click += new EventHandler(PicClickEvent_Click);
+                    PlayOne[i].pic.Name = PlayOne[i].sName;
+                    PlayOne[i].pic.Tag = i;
+                }
+                if (PlayTwo[i].pic != null)
+                {
+                    PlayTwo[i].pic.Click += new EventHandler(PicClickEvent_Click);
+                    PlayTwo[i].pic.Name = PlayTwo[i].sName;
+                    PlayTwo[i].pic.Tag = i + 16;
+                }
+            }
+        }
+
+        public void PicClickEvent_Click(object sender, EventArgs e)
+        {
+            PictureBox Pic = sender as PictureBox;
+
+            if (null == Pic) 
+                return;
+
+            if (Global.iBordType == (int)GameBoard.HalfBoard)
+            {
+                //Console.WriteLine(Pic.Tag);
+                if ((int)Pic.Tag < 16)
+                {
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if ((int)PlayOne[i].pic.Tag == (int)Pic.Tag)
+                        {
+                            // 選棋
+                            // 同一支棋
+                            if (PlayOne[i].ePieceSatus == SelectedType.Selected)
+                            {
+                                PlayOne[i].ePieceSatus = SelectedType.None;
+
+                                FightSeletedPiece[0] = null;
+                                FightSeletedPiece[1] = null;
+                                Global.MsgStatusUpdate("不選取 " + PlayOne[i].sName);
+                                Console.WriteLine("PlayOne " + Pic.Name + " Tag " + (int)Pic.Tag + " Status " + PlayOne[i].ePieceSatus.ToString());
+                                break;
+                            }
+
+                            // 點擊棋後改狀態
+                            if (PlayOne[i].ePieceSatus == SelectedType.None && PlayOne[i].ePieceSatus != SelectedType.Selected)
+                            {
+                                PlayOne[i].ePieceSatus = SelectedType.Selected;
+                                
+                                if(bFight == false)
+                                {
+                                    FightSeletedPiece[0] = PlayOne[i];
+                                    FightSeletedPiece[1] = null;
+                                }
+                                bFight = true;
+                                Global.MsgStatusUpdate("選取 " + PlayOne[i].sName);
+                                Console.WriteLine("PlayOne " + Pic.Name + " Tag " + (int)Pic.Tag + " Status " + PlayOne[i].ePieceSatus.ToString());
+                                break;
+                            }
+                        }
+                    }
+                } // end PlayOne
+                if ((int)Pic.Tag > 15)
+                {
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if ((int)PlayTwo[i].pic.Tag == (int)Pic.Tag)
+                        {
+                            // 選棋
+                            // 同一支棋
+                            if (PlayTwo[i].ePieceSatus == SelectedType.Selected)
+                            {
+                                PlayTwo[i].ePieceSatus = SelectedType.None;
+
+                                FightSeletedPiece[0] = null;
+                                FightSeletedPiece[1] = null;
+                                bFight = false;
+                                Global.MsgStatusUpdate("不選取 " + PlayTwo[i].sName);
+                                Console.WriteLine("PlayTwo " + Pic.Name + " Tag " + (int)Pic.Tag + " Status " + PlayOne[i].ePieceSatus.ToString());
+                            }
+                            // 點擊棋後改狀態
+                            if (PlayTwo[i].ePieceSatus == SelectedType.None && PlayTwo[i].ePieceSatus != SelectedType.Selected)
+                            {
+                                PlayTwo[i].ePieceSatus = SelectedType.Selected;
+                                
+                                if(bFight == false)
+                                {
+                                    FightSeletedPiece[0] = PlayTwo[i];
+                                    FightSeletedPiece[1] = null;
+                                }
+                                bFight = true;
+                                Global.MsgStatusUpdate("選取 " + PlayTwo[i].sName);
+                                Console.WriteLine("PlayTwo " + Pic.Name + " Tag " + (int)Pic.Tag + " Status " + PlayTwo[i].ePieceSatus.ToString());
+                            }
+                        }
+                    }
+                } // end PlayTwo
+            } // end HalfBoard
+        }
+
+        // 切割大圖到PlayOne PlayTwo
+        public void PaintPieceIcon()
+        {
+            /*
+            pictureBox1.Image = originalImage.Clone(rect, originalImage.PixelFormat);
+             */
+            Bitmap originalImage = new Bitmap(Properties.Resources.Piece);
+            for (int i = 0; i < 16; i++)
+            {
+                Rectangle rect = new Rectangle(i * sPieceIcon.Width, 0, sPieceIcon.Width, sPieceIcon.Height);
+                PlayOne[i].pic.Image = originalImage.Clone(rect, originalImage.PixelFormat);
+                PlayOne[i].pic.Size = sPieceIcon;
+
+                Rectangle rect1 = new Rectangle(i * sPieceIcon.Width, sPieceIcon.Height, sPieceIcon.Width, sPieceIcon.Height);
+                PlayTwo[i].pic.Image = originalImage.Clone(rect1, originalImage.PixelFormat);
+                PlayTwo[i].pic.Size = sPieceIcon;
+            }
+        }
+    }
+}
